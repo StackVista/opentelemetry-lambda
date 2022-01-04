363208d2e04e8a6365b7139d0497836bb53a9734
Melcom van Eeden <melcomve@outlook.com>
2022-01-04 21:10:42 +0200

    Various changes for the layer usage, deploy and agent

--- nodejs/sample-apps/sts-agent/otel-agent-deploy-run.sh
+++ nodejs/sample-apps/sts-agent/otel-agent-deploy-run.sh
@@ -2,5 +2,5 @@ docker load -i otel-agent.docker
 
 AGENT_ENDPOINT=${AGENT_ENDPOINT} \
   STS_API_KEY="${STS_API_KEY}" \
-  AGENT_BRANCH="${AGENT_BRANCH}" \
+  STACKSTATE_ENDPOINT="${STACKSTATE_ENDPOINT}" \
   docker-compose --file otel-agent-docker-compose.yml up
